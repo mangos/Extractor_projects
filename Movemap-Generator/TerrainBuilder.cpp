073c72b5f638bf36808091b5a513fa790983716b
Meltie2013 <Meltie2013@users.noreply.github.com>
2023-07-26 04:35:46 -0500

    Build ADT floor like WMO floor below liquid (#22)

--- Movemap-Generator/TerrainBuilder.cpp
+++ Movemap-Generator/TerrainBuilder.cpp
@@ -523,12 +523,6 @@ namespace MMAP
                         }
                     }
 
-                    // terrain under the liquid?
-                    if (minLLevel > maxTLevel)
-                    {
-                        useTerrain = false;
-                    }
-
                     //liquid under the terrain?
                     if (minTLevel > maxLLevel)
                     {
